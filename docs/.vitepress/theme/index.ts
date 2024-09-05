5eedc10ecc16bd9917c38b24f3bbef5ca5d370e8
Sravan Goud <tsravangoud164@gmail.com>
2024-09-05 13:28:21 +0530

    added giscus plugin

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,9 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+import { toRefs } from "vue";
 import './style.css'
 
 export default {
@@ -13,5 +16,29 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup() {
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+    
+    giscusTalk({
+        repo: 'tsra0ne/theswiftguy',
+        repoId: 'R_kgDOMtGVzg',
+        category: 'General',
+        categoryId: 'DIC_kwDOMtGVzs4CiNtC',
+        mapping: 'pathname',
+        inputPosition: 'top',
+        lang: 'en',
+        locales: {
+            'en-US': 'en'
+        },
+        homePageShowComment: false,
+        lightTheme: 'light',
+        darkTheme: 'transparent_dark',
+    }, {
+        frontmatter, route
+    },
+        true
+    );
   }
 } satisfies Theme
