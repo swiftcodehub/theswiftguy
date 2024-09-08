05f921896e0838c37a1067c15dd80023f4fc269f
Sravan Goud <tsravangoud164@gmail.com>
2024-09-08 08:47:31 +0530

    Revert "added rss"
    This reverts commit 17186a7341b52226f33690311bb5ff6182fec690.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,9 @@
-import path from 'path'
-import { writeFileSync } from 'fs'
-import { Feed } from 'feed'
-import { defineConfig, createContentLoader, type SiteConfig } from 'vitepress'
-
-const hostname: string = 'https://theswiftguy.in'
+import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   lang: 'en-US',
   title: "The Swift Guy",
-  description: "Learn SwiftUI and UIKit",
+  description: "Learn Swift, SwiftUI and UIKit",
   cleanUrls: true,
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
@@ -54,51 +49,5 @@ export default defineConfig({
     search: {
       provider: "local",
     }
-  },
-  buildEnd: async (config: SiteConfig) => {
-    const feed = new Feed({
-      title: 'The Swift Guy',
-      description: 'Learn SwiftUI and UIKit',
-      id: hostname,
-      link: hostname,
-      language: 'en',
-      image: 'https://theswiftguy.in/logo.png',
-      favicon: `${hostname}/favicon.ico`,
-      copyright:
-        'Copyright (c) 2024-present, The Swift Guy'
-    })
-
-    // You might need to adjust this if your Markdown files 
-    // are located in a subfolder
-    const posts = await createContentLoader('*.md', {
-      excerpt: true,
-      render: true
-    }).load()
-  
-    posts.sort(
-      (a, b) =>
-        +new Date(b.frontmatter.date as string) -
-        +new Date(a.frontmatter.date as string)
-    )
-  
-    for (const { url, excerpt, frontmatter, html } of posts) {
-      feed.addItem({
-        title: frontmatter.title,
-        id: `${hostname}${url}`,
-        link: `${hostname}${url}`,
-        description: excerpt,
-        content: html,
-        author: [
-          {
-            name: 'Sravan Goud',
-            email: 'tsravangoud164@gmail.com',
-            link: 'https://theswiftguy.in/developers'
-          }
-        ],
-        date: frontmatter.date
-      })
-    }
-  
-    writeFileSync(path.join(config.outDir, 'feed.rss'), feed.rss2())
   }
 })
